801a57a80cc65680989c52b9f7e3c4e7453c019d
Borna Ivankovic <bivankovi@protonmail.com>
2025-03-27 23:15:57 +0100

    Tests are properly linked

--- test/Exam.cpp
+++ test/Exam.cpp
@@ -1,6 +1,9 @@
 #include <gtest/gtest.h>
+#include "FakeQuestionGenerator.h"
 
 // TODO: MOVE SHARED LIBRARY TO THE SAME LOCATION AS THE TEST EXECUTABLE !!!
+
+
 
 TEST(Example, willRun){
   EXPECT_EQ("UNS", "RUNS");
