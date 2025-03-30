6b55f36c6aa96e46ce8a3cd73573a838fc4f85b8
Borna Ivankovic <bivankovi@protonmail.com>
2025-03-30 11:02:53 +0200

    Fixed google test not having main function, excluded wrap files from .gitignore

--- test/Exam.cpp
+++ test/Exam.cpp
@@ -3,8 +3,11 @@
 
 // TODO: MOVE SHARED LIBRARY TO THE SAME LOCATION AS THE TEST EXECUTABLE !!!
 
-
-
 TEST(Example, willRun){
   EXPECT_EQ("UNS", "RUNS");
+}
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
